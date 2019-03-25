f404f025d4286f50b7dbbf4167e8c57c8ea587ba
padraic wade <padraic.wade@woodgroup.com>
2019-03-25 17:40:09 +0000

    start adding 256 boilerplates

--- sha254.c
+++ sha254.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+
 int main()
 {
-  printf("hello world\n");
-  return 0;
+  sha256();
+}
+
+sha256(){
+
+
+
 }
