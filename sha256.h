bcb9ce4259a0d304082021e886903710e554690f
padraic wade <padraic.wade@woodgroup.com>
2019-03-30 12:50:52 +0000

    modify header

--- sha256.h
+++ sha256.h
@@ -2,6 +2,7 @@
 #define _SHA256_H
  
 #include <stdint.h>
-#define SHA256_DIGEST_SIZE (256/8)
 
-void Sha256_Init(sha256Context *context);
+// FIPS PUB 180-4 -- Figure 1
+// SHA256 outputs 256 bits digest
+#define SHA256_DIGEST_SIZE (256/8)
